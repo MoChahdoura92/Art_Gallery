940b17a66d1b369cf0187225fa59ab80f987cb95
Mohamed Chahdoura <chahdoura.mohamed6@gmail.com>
2024-06-23 20:08:08 +0200

    tailwind_final

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import "./Header.css";
 
 export default function Header() {
   return (
     <header>
       <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
-      <p>A community of artists and art-lovers.</p>
+      <h1 className="text-3xl font-bold underline">ReactArt</h1>
+      <p className="font-semibold text-indigo-600 text-gray-100">
+        A community of artists and art-lovers.
+      </p>
     </header>
   );
 }
